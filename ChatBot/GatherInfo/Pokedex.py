48d6b1ef3cf5ff73d38111462095716b162a7126
Chris Janusa <chrisjanusa@yahoo.com>
2018-11-19 17:28:00 -0600

    Added pokedex info

---
+++ ChatBot/GatherInfo/Pokedex.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import pickle
+
+
+def main():
+    cols = ["against_bug", 'against_dark', "against_dragon", "against_electric", "against_fairy", "against_fight",
+            "against_fire", "against_flying", "against_ghost", "against_grass", "against_ground", "against_ice",
+            "against_normal", "against_poison", "against_psychic", "against_rock", "against_steel", "against_water",
+            "name", "pokedex_number", "type1", "type2", "generation"]
+
+    df = pd.read_csv('pokemon.csv', skipinitialspace=True, usecols=cols)
+    pokedex = {}
+    for row in df.itertuples():
+        temp = {}
+        for i, col in enumerate(cols, start=1):
+            temp[col] = row[i]
+        print(temp)
+        pokedex[row.name] = temp
+    pickle.dump(pokedex, open("../pokedex.pickle", "wb"))
+
+
+if __name__ == "__main__":
+        main()
