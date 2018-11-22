b3b6574c2c89da6fa18c90274c8683eaaf2f545d
Chris Janusa <cmj160130@utdallas.edu>
2018-11-21 20:31:08 -0600

    Working Shiny Pattern (#15)

--- ChatBot/Find.py
+++ ChatBot/Find.py
@@ -30,37 +30,44 @@ def proccess_sentance(sent):
     parse = Parsed()
     parse.pronoun = find_pronoun(nlp_sent)
     parse.adj = find_adjective(nlp_sent)
-    parse.noun = find_noun(nlp_sent)
+    parse.subj = find_subj(nlp_sent)
     parse.verb = find_verb(nlp_sent)
+    parse.doj = find_doj(nlp_sent)
     parse.name = find_name(nlp_sent)
     parse.pokemon = find_pokemon(nlp_sent)
     parse.imp_terms = find_imp_term(nlp_sent)
     parse.team = find_team(nlp_sent)
     parse.isFarewell = isFarewell(nlp_sent)
+    logger.info("Dobj: %s Pronouns: %s Adjs: %s Subjs: %s Verbs: %s Names: %s Pokemon: %s Imp Terms: %s Team: %s",
+                ", ".join(parse.doj), "".join(parse.pronoun), ", ".join(parse.adj), ", ".join(parse.subj), ", ".join(parse.verb),
+                parse.name, ", ".join(parse.pokemon), ", ".join(parse.imp_terms), parse.team)
+
     return parse
 
 
 def find_pronoun(sent):
     """Given a sentence, find a preferred pronoun to respond with. Returns None if no candidate
     pronoun is found in the input"""
+    pronouns = []
     for word in sent:
         # Disambiguate pronouns
-        if word.pos_ == 'PRP' and word.text.lower() == 'you':
+        if word.tag_ == 'PRP' and word.text.lower() == 'you':
             logger.info("Found pronoun: I")
-            return 'I'
-        elif word.pos_ == 'PRP' and word.text == 'I':
+            pronouns.append('I')
+        elif word.tag_ == 'PRP' and word.text == 'I':
             logger.info("Found pronoun: You")
-            return 'You'
+            pronouns.append('You')
     return ""
 
 
 def find_verb(sent):
     """Pick a candidate verb for the sentence."""
+    verbs = []
     for word in sent:
-        if word.pos_.startswith('VB'):  # This is a verb
-            logger.info("Found verb: %s", word.text)
-            return word.text
-    return ""
+        if word.tag_.startswith('VB'):  # This is a verb
+            logger.info("Found verb: %s Lemma: %s", word.text, word.lemma_)
+            verbs.append(str(word.lemma_))
+    return verbs
 
 
 def find_name(sent):
@@ -78,42 +85,55 @@ def find_name(sent):
     return ""
 
 
-def find_noun(sent):
+def find_subj(sent):
     """Given a sentence, find the best candidate noun."""
+    subjs = []
     for word in sent:
-        if word.pos_ == 'NN':  # This is a noun
-            logger.info("Found noun: %s", word.text)
-            return word.text
-    return ""
+        if word.dep_ == 'subj':  # This is a noun
+            logger.info("Found subject: %s Lemma: %s", word.text, word.lemma_)
+            subjs.append(str(word.lemma_))
+    return subjs
+
+
+def find_doj(sent):
+    """Given a sentence, find the best candidate noun."""
+    dobjs = []
+    for word in sent:
+        if word.dep_ == 'dobj':  # This is a noun
+            logger.info("Found dobj: %s Lemma: %s", word.text, word.lemma_)
+            dobjs.append(str(word.lemma_))
+    return dobjs
 
 
 def find_adjective(sent):
     """Given a sentence, find the best candidate adjective."""
+    adjs = []
     for word in sent:
-        if word.pos_ == 'JJ':  # This is an adjective
-            logger.info("Found adjective: %s", word.text)
-            return word.text
-    return ""
+        if word.tag_ == 'JJ':  # This is an adjective
+            logger.info("Found adj: %s Lemma: %s", word.text, word.lemma_)
+            adjs.append(word.lemma_)
+    return adjs
 
 
 def find_pokemon(sent):
     """Given a sentence, find if a user mentioned a pokemon."""
     pokemons = []
     for token in sent:
+        if token.text == 'shiny':
+            continue
+
         closest = ""
         close_dist = 3
-        for pokemon in POKEMON_AVAIL:
-            dist = Levenshtein.distance(token.text.lower(), pokemon.lower())
-            if dist < close_dist:
-                closest = pokemon
-                close_dist = dist
-        for pokemon in POKEMON_NOT_AVAIL:
+        if len(token.text) < 5:
+            closest = ""
+            close_dist = 1
+        for pokemon in ALL_POKEMON:
             dist = Levenshtein.distance(token.text.lower(), pokemon.lower())
             if dist < close_dist:
                 closest = pokemon
                 close_dist = dist
             # logger.info("Token %s has distance %d from %s", token, dist, pokemon)
-        if close_dist < 3:
+        if closest != "":
             logger.info("Found pokemon: %s", closest)
             pokemons.append(closest)
     return pokemons
@@ -160,7 +180,7 @@ def find_pokemon_fact(pokemon):
         strong_against = ""
         weak_against = ""
         # if pokedex[pokemon] != "":
-        if isinstance(pokedex[pokemon]["type2"], str) :
+        if isinstance(pokedex[pokemon]["type2"], str):
             pok_type += "/" + pokedex[pokemon]["type2"]
         facts.append(random.choice(TYPE_RESPONSES).format(**{'pokemon': pokemon, 'type':pok_type}))
 
