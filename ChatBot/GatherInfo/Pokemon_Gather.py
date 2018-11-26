eabe40506b7f38d8b58b1cb910162001df951409
Chris Janusa <chrisjanusa@yahoo.com>
2018-11-25 21:15:22 -0600

    Comment pokemon_gather

--- ChatBot/GatherInfo/Pokemon_Gather.py
+++ ChatBot/GatherInfo/Pokemon_Gather.py
@@ -5,24 +5,35 @@ from pathlib import Path
 
 
 def main():
+    # Prep website for scraping
     u = "https://serebii.net/pokemongo/pokemon.shtml"
-
     webpage = requests.get(u).text
     soup = BeautifulSoup(webpage, features="html.parser")
     data = str(soup.findAll()).split("\n")
+
+    # Create lists to store pokemon of different overlapping types
     pokemon_avail = []
     pokemon_not_avail = []
     has_alola = []
     regionals = []
     for line in data:
         if line.startswith("<a href=\"/pokemongo/pokemon/") and ">- In-Depth" not in line:
+            # Find start of pokemon name
             start = len("<a href=\"/pokemongo/pokemon/031.shtml\">")
+
+            # Find end of pokemon name
             end = line.find("</a>", start)
+
+            # Remove male and female signs due to crash on windows computers in handling them
             if line.find(u"\u2642") != -1:
                 end = line.find(u"\u2642")
             if line.find(u"\u2640") != -1:
                 end = line.find(u"\u2640")
+
+            # Get pokemon name
             pokemon = line[start:end]
+
+            # Add to appropriate lists based on tags in the line
             if "<i>Not currently available</i>" in line:
                 pokemon_not_avail.append(pokemon)
             else:
@@ -31,12 +42,15 @@ def main():
                 if "Only available in" in line:
                     regionals.append(pokemon)
                 pokemon_avail.append(pokemon)
+
+    # Shiny list must be gathered from different website and this function handles that
     shiny = get_shiny_list()
 
     outfile = Path("../Info/Pokemon.py")
     if not outfile.is_file():
         outfile = Path("Info/Pokemon.py")
 
+    # Write all information to Pokemon.py file
     with open(str(outfile), "w") as pokemon_file:
         pokemon_file.write("POKEMON_AVAIL = {\n")
         first = True
@@ -90,6 +104,7 @@ def main():
 
 
 def get_shiny_list():
+    # Shiny list must be gathered from different website
     u = "https://www.imore.com/pokemon-go-shiny"
 
     webpage = requests.get(u).text
@@ -101,6 +116,7 @@ def get_shiny_list():
 
 
 def clean_line(line):
+    # Find the name of the pokemon from the line
     print_next = False
     for word in re.split('[ ><]', str(line)):
         if print_next:
